1f22e14fc5dc65d083a463b4d07f185072951f2f
primitive.snake <primitive.snake@localhost>
2010-12-16 00:15:21 -0500

    dic 16, informe avance poa terminado, pendiente informe ejecucion semanal

--- indicadores/lib/model/ActividadProcedimientoPoa.php
+++ indicadores/lib/model/ActividadProcedimientoPoa.php
@@ -12,4 +12,47 @@ class ActividadProcedimientoPoa extends BaseActividadProcedimientoPoa
   public function  __toString() {
     return $this->getActividad();
   }
+
+    /**
+   * Calcula la ejecucion de la actividad a partir de los avances de las subactividades
+   * relacionadas
+   */
+  public function getEjecucion()
+  {
+    // consultar subactividades relacionadas
+    $c = new Criteria();
+    $c->add(SubactividadProcedimientoPoaPeer::ACTIVIDAD_PROCEDIMIENTO_ID, $this->id);
+
+    // revisar por si algo no funciona
+    $resultset = SubactividadProcedimientoPoaPeer::doSelectJoinAll($c);
+
+    $ejecucion = 0;
+    foreach ($resultset as $item)
+    {
+      $ejecucion += $item->getEjecucionPonderada();
+    }
+
+    return $ejecucion;
+  }
+
+  public function getEjecucionPonderada()
+  {
+    return ($this->getPonderacion()/100)*$this->getEjecucion();
+  }
+
+  public function getPonderacionAcum()
+  {
+    $c = new Criteria();
+    $c->add(SubactividadProcedimientoPoaPeer::ACTIVIDAD_PROCEDIMIENTO_ID, $this->id);
+
+    $subactividades = SubactividadProcedimientoPoaPeer::doSelect($c);
+    $sum = 0;
+
+    foreach ($subactividades as $subactividad)
+    {
+      $sum += $subactividad->getPonderacion();
+    }
+
+    return $sum;
+  }
 }
