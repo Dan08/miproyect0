edc5659abd98c5ee40d031d7076945c3b62b3f71
primitive.snake <primitive.snake@localhost>
2010-12-16 00:15:21 -0500

    dic 16, informe avance poa terminado, pendiente informe ejecucion semanal

--- indicadores/apps/indicadores/modules/informes/templates/PoaejecucionSuccess.php
+++ indicadores/apps/indicadores/modules/informes/templates/PoaejecucionSuccess.php
@@ -3,5 +3,58 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-var_dump($actividades);
+  $cuenta = array();
+  foreach ($actividades as $actividad)
+  {
+    $cuenta[$actividad->getActividadProcedimientoId()]++;
+  }
 ?>
+
+<br />
+<h3>Informe Avance Procedimiento</h3>
+<br />
+
+<table>
+<thead>
+<tr>
+  <th>Actividad</th>
+  <th>% Ejecucion</th>
+  <th>% Ponderacion</th>
+  <th>% Ejecucion Ponderada</th>
+  <th>Subactividad</th>
+  <th>% Ejecucion</th>
+  <th>% Ponderacion</th>
+  <th>% Ejecucion Ponderada</th>
+</tr>
+</thead>
+<tbody>
+  <?php $pos = $actividades[0]->getId(); $total = 0; $pond = 0; ?>
+  <?php foreach ($actividades as $actividad): ?>
+  <tr>
+    <?php if ($pos == (pos($actividad))): ?>
+      <td rowspan="<?php echo current($cuenta) ?>"><?php echo $actividad->getActividadProcedimientoPoa()->getActividad() ?></td>
+      <td rowspan="<?php echo current($cuenta) ?>"><?php echo $actividad->getActividadProcedimientoPoa()->getEjecucion() ?></td>
+      <td rowspan="<?php echo current($cuenta) ?>"><?php echo $actividad->getActividadProcedimientoPoa()->getPonderacion() ?></td>
+      <td rowspan="<?php echo current($cuenta) ?>"><?php echo $actividad->getActividadProcedimientoPoa()->getEjecucionPonderada() ?></td>
+      <?php $pos +=current($cuenta); next($cuenta); ?>
+    <? endif; ?>
+      <td><?php echo $actividad->getDescripcion() ?></td>
+      <td><?php echo $actividad->getEjecucion() ?></td>
+      <?php $total += $actividad->getPonderacion() ?>
+      <td><?php echo $actividad->getPonderacion() ?></td>
+      <?php $pond += $actividad->getEjecucionPonderada() ?>
+      <td><?php echo $actividad->getEjecucionPonderada() ?></td>
+  </tr>
+   <?php if ($pos == (pos($actividad)+1)): ?>
+    <tr>
+      <td colspan="6" align="center"><strong>Total Actividad</strong></td>
+      <td><strong><?php echo $total; $total = 0; ?></strong></td>
+      <td><strong><?php echo $pond; $pond = 0; ?></strong></td>
+    </tr>
+  <?php endif; ?>
+  <?php endforeach; ?>
+</tbody>
+</table>
+
+
+  
--- indicadores/lib/model/ActividadPoa.php
+++ indicadores/lib/model/ActividadPoa.php
@@ -13,47 +13,4 @@ class ActividadPoa extends BaseActividadPoa
   {
     return $this->getDescripcion();
   }
-
-  /**
-   * Calcula la ejecucion de la actividad a partir de los avances de las subactividades
-   * relacionadas
-   */
-  public function getEjecucion()
-  {
-    // consultar subactividades relacionadas
-    $c = new Criteria();
-    $c->add(SubactividadPoaPeer::ACTIVIDAD_POA_ID, $this->id);
-
-    // revisar por si algo no funciona
-    $resultset = SubactividadPoaPeer::doSelectJoinAll($c);
-
-    $ejecucion = 0;
-    foreach ($resultset as $item)
-    {
-      $ejecucion += $item->getEjecucionPonderada();
-    }
-
-    return $ejecucion;
-  }
-
-  public function getEjecucionPonderada()
-  {
-    return ($this->getPonderacion()/100)*$this->getEjecucion();
-  }
-
-  public function getPonderacionAcum()
-  {
-    $c = new Criteria();
-    $c->add(SubactividadPoaPeer::ACTIVIDAD_POA_ID, $this->id);
-
-    $subactividades = SubactividadPoaPeer::doSelect($c);
-    $sum = 0;
-
-    foreach ($subactividades as $subactividad)
-    {
-      $sum += $subactividad->getPonderacion();
-    }
-
-    return $sum;
-  }
 }
